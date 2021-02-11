aec5edd668942707d8831b0de3ac65e5f0a5f13f
ying-jeanne <wangyingensea@hotmail.com>
2021-02-11 23:43:31 +0100

    add trim

---
+++ src/cuety/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bytes"
+	gojson "encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"export"
+
+	"cuelang.org/go/cue"
+)
+
+func getJSONContent(inputJSONFileName string) map[string]interface{} {
+	var result map[string]interface{}
+	// Open jsonFile
+	jsonFile, err := os.Open(inputJSONFileName)
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+	}
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	gojson.Unmarshal([]byte(byteValue), &result)
+	return result
+}
+
+func getCueRootValue(inputCUEFileName string) cue.Value {
+	spec, _ := ioutil.ReadFile(inputCUEFileName)
+	config := string(spec)
+	var r cue.Runtime
+	rinstance, _ := r.Compile("test", config)
+	return rinstance.Value()
+}
+
+func writeResultToFile(result *map[string]interface{}, resultFileName string) {
+	jsonString, _ := gojson.MarshalIndent(result, "", "    ")
+	_ = ioutil.WriteFile(resultFileName, jsonString, 0644)
+}
+
+func compareResults(res string, ref string) bool {
+	one, err := ioutil.ReadFile(res)
+	if err != nil {
+		panic(err)
+	}
+	two, err2 := ioutil.ReadFile(ref)
+	if err2 != nil {
+		panic(err2)
+	}
+	return bytes.Equal(one, two)
+}
+
+func addDefaultValue(res *map[string]interface{}, rvalue cue.Value) {
+
+}
+
+var (
+	refereceFileName  string = "reference.json"
+	resultFileName    string = "output.cue"
+	inputJSONFileName string = "test.json"
+	inputCUEFileName  string = "test.cue"
+	inputPackageName  string = "cuety"
+)
+
+func main() {
+	// result := getJSONContent(inputJSONFileName)
+	// rvalue := getCueRootValue(inputCUEFileName)
+	export.RemoveDefaultValue(inputPackageName)
+	// fmt.Println(result)
+	// writeResultToFile(&result, resultFileName)
+	// isEqual := compareResults(resultFileName, refereceFileName)
+	// if isEqual {
+	// fmt.Println("Test pass")
+	// } else {
+	// fmt.Println("Test fail")
+	// }
+	// addDefaultValue(&result, rvalue)
+	// writeResultToFile(&result, "defaultback.json")
+}
---
+++ src/export/export.go
@@ -0,0 +1,82 @@
+package export
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"diff"
+
+	"cuelang.org/go/cue"
+	"cuelang.org/go/cue/format"
+	"cuelang.org/go/cue/load"
+	"cuelang.org/go/tools/trim"
+)
+
+var forcetrim = false
+var simplify = false
+
+/*RemoveDefaultValue is taking the destination file name as input,
+and remove the default value from json input. */
+func RemoveDefaultValue(dst string) {
+	overlay := map[string]load.Source{}
+	loadedInstances := load.Instances([]string{"."}, &load.Config{Package: "cuety", Overlay: overlay})
+	instances := cue.Build(loadedInstances)
+
+	for i, inst := range loadedInstances {
+		root := instances[i]
+		err := trim.Files(inst.Files, root, &trim.Config{
+			Trace: true,
+		})
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		for _, f := range inst.Files {
+			overlay[f.Filename] = load.FromFile(f)
+		}
+
+	}
+
+	tinsts := instances
+	if len(tinsts) != len(loadedInstances) {
+		fmt.Println("unexpected number of new instances")
+	}
+
+	if forcetrim {
+		for i, p := range instances {
+			k, script := diff.Final.Diff(p.Value(), tinsts[i].Value())
+			if k != diff.Identity {
+				diff.Print(os.Stdout, script)
+				fmt.Println("Aborting trim, output differs after trimming. This is a bug! Use -i to force trim.")
+				fmt.Println("You can file a bug here: https://github.com/cuelang/cue/issues/new?assignees=&labels=NeedsInvestigation&template=bug_report.md&title=")
+				os.Exit(1)
+			}
+		}
+	}
+
+	for _, inst := range loadedInstances {
+		for _, f := range inst.Files {
+			filename := f.Filename
+
+			opts := []format.Option{}
+			if simplify {
+				opts = append(opts, format.Simplify())
+			}
+
+			b, err := format.Node(f, opts...)
+			if err != nil {
+				fmt.Errorf("error formatting file: %v", err)
+			}
+
+			if dst != "" {
+				filename = dst
+			}
+
+			err = ioutil.WriteFile(filename, b, 0644)
+			if err != nil {
+				fmt.Errorf("writing file failed: ", err)
+			}
+		}
+	}
+}
